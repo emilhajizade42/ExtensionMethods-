1eaaf0a43bece64732530d20605fe8566ca381c1
emilhajizade42 <6xp3xcg@code.edu.az>
2022-04-05 10:51:18 +0400

    task done

---
+++ ExtensionMethods/Extensions/ExtensionMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtensionMethods.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static bool CustomContains(this string str, char chr)
+        {
+            foreach (var item in str)
+            {
+                if (item==chr)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        public static bool CustomContains(this string str, string strPart)
+        {
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]==strPart[0])
+                {
+                    for (int j = 0; j < strPart.Length; j++)
+                    {
+                        if (str[i+j]==strPart[j])
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            count = 0;
+                            break;
+                        }
+                    }
+                    if (count == strPart.Length)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        count = 0;
+
+                    }
+
+                }
+            }
+
+            return false;
+        }
+        public static bool IsPrime(this int num)
+        {
+            if (num<1)
+            {
+                return false;
+            }
+            for (int i = 2; i*i < num; i++)
+            {
+                if (num%i==0)
+                {
+                    return false;
+                }
+                
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExtensionMethods.Extensions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var word = "salam";
+            var word1 =  word.CustomContains('s');
+            var word2 = word.CustomContains("alza");
+
+            Console.WriteLine(word1);
+            Console.WriteLine(word2);
+
+            var num = 13 ;
+            var num2 = num.IsPrime();
+
+            Console.WriteLine(num2);
+        }
+    }
+}
